9e4781eae80275499766b719cfa3d409c49cb4f2
João Vitor Vieira Contin <nukezzzin@gmail.com>
2026-03-24 14:16:03 -0300

    [feat] prevent login when already logged in

--- lib/web/controllers/account_controller.ex
+++ lib/web/controllers/account_controller.ex
@@ -5,19 +5,25 @@ defmodule PrometheusEntry.Controllers.AccountController do
 
   @spec register(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def register(connection, parameters) when is_map(parameters) and map_size(parameters) > 0 do
-    case AccountContext.register_user(parameters) do
-      {:ok, tuple_tokens} ->
-        connection
-        |> put_status(:created)
-        |> json(%{success: true, data: tuple_tokens})
-      {:error, %Ecto.Changeset{} = changeset} ->
-        connection
-        |> put_status(:unprocessable_entity)
-        |> json(%{success: false, errors: format_changeset_errors(changeset)})
-      _ ->
-        connection
-        |> put_status(:internal_server_error)
-        |> json(%{success: false, errors: [%{code: "INTERNAL_SERVER_ERROR", message: "Unexpected error"}]})
+    if connection.assigns[:current_user] != nil do
+      connection
+      |> put_status(:forbidden)
+      |> json(%{success: false, errors: [%{code: "FORBIDDEN", message: "You are already logged in"}]})
+    else
+      case AccountContext.register_user(parameters) do
+        {:ok, tuple_tokens} ->
+          connection
+          |> put_status(:created)
+          |> json(%{success: true, data: tuple_tokens})
+        {:error, %Ecto.Changeset{} = changeset} ->
+          connection
+          |> put_status(:unprocessable_entity)
+          |> json(%{success: false, errors: format_changeset_errors(changeset)})
+        _ ->
+          connection
+          |> put_status(:internal_server_error)
+          |> json(%{success: false, errors: [%{code: "INTERNAL_SERVER_ERROR", message: "Unexpected error"}]})
+      end
     end
   end
 
@@ -26,15 +32,21 @@ defmodule PrometheusEntry.Controllers.AccountController do
 
   @spec login(Plug.Conn.t(), %{identifier: String.t(), password: String.t()}) :: Plug.Conn.t()
   def login(connection, %{"identifier" => identifier, "password" => password}) when is_binary(identifier) and is_binary(password) do
-    case AccountContext.login_user(identifier, password) do
-      {:ok, tuple_tokens} ->
-        connection
-        |> put_status(:ok)
-        |> json(%{success: true, data: tuple_tokens})
-      _ ->
-        connection
-        |> put_status(:unauthorized)
-        |> json(%{success: false, errors: [%{code: "INVALID_CREDENTIALS", message: "Invalid credentials"}]})
+    if connection.assigns[:current_user] != nil do
+      connection
+      |> put_status(:forbidden)
+      |> json(%{success: false, errors: [%{code: "FORBIDDEN", message: "You are already logged in"}]})
+    else
+      case AccountContext.login_user(identifier, password) do
+        {:ok, tuple_tokens} ->
+          connection
+          |> put_status(:ok)
+          |> json(%{success: true, data: tuple_tokens})
+        _ ->
+          connection
+          |> put_status(:unauthorized)
+          |> json(%{success: false, errors: [%{code: "INVALID_CREDENTIALS", message: "Invalid credentials"}]})
+      end
     end
   end
 
