f538af1c3c30ee6a1028eff613abc5e5501454e8
João Vitor Vieira Contin <nukezzzin@gmail.com>
2026-03-24 11:31:16 -0300

    [feat] add seeds.exs with initial user and post

--- priv/repository/seeds.exs
+++ priv/repository/seeds.exs
@@ -1,15 +1,18 @@
-Prometheus.Repository.insert!(%Prometheus.Schemas.UserSchema{}, %{
+%Prometheus.Schemas.UserSchema{}
+|> Prometheus.Schemas.UserSchema.create_user_changeset(%{
   id: 1,
-  user_flags: 0b1,
   username: "jcontin",
   display_name: "João Vitor Vieira Contin",
   email: "nukezzzin@gmail.com",
-  password: "$uper$ecretPassw0rd",
+  password: "$uper$ecretPassw0rd"
 })
+|> Prometheus.Repository.insert!()
 
-Prometheus.Repository.insert!(%Prometheus.Schemas.PostSchema{}, %{
+%Prometheus.Schemas.PostSchema{}
+|> Prometheus.Schemas.PostSchema.create_post_changeset(%{
   id: 1,
-  user_id: 1,
+  author_id: 1,
   title: "Primeiro Post",
   content: "Esté é o primeiro post.",
 })
+|> Prometheus.Repository.insert!()
