4cd64894e4f51ca8e4ec3c49bee29140c3cc5c85
João Vitor Vieira Contin <nukezzzin@gmail.com>
2026-03-25 00:21:22 -0300

    [chore] include regex to accept all tunnel ngrok

--- lib/web/endpoint.ex
+++ lib/web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule PrometheusEntry.Endpoint do
   ]
 
   @cors_options [
-    origins: ["http://localhost:5173"],
+    origins: ["http://localhost:5173", ~r/^https:\/\/.*\.ngrok\.io$/],
     allow_methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers: ["authorization", "content-type", "accept"],
     allow_credentials: true,
